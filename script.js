eb639e8937271bda073c1dd05fc5cc0b914b58b6
Shamim Dewan <570iamkhan@gmail.com>
2021-10-02 10:01:14 +0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+function colorGenerate(){
+    let hexcodes = ['0','1','2','3','4','5','6','7','8','9','a','b','c','d','e','f'];
+    let color = '#';
+    for(let i = 0;i < 6;i++){
+        let num = Math.round(Math.random() * 15);
+        let num2 = hexcodes[num];
+        color += num2;
+    }
+    document.getElementById('container').style.background = color;
+    document.getElementById('output').style.color = color;
+    document.getElementById('output').innerHTML = color;   
+}
